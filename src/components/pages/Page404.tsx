dab87938b51f22a42e55e58508c5372c7be0d4cc
kkosuke <kkosuke007@gmail.com>
2023-02-24 22:13:55 +0900

    react-router-dom v6 設定

---
+++ src/components/pages/Page404.tsx
@@ -0,0 +1,5 @@
+import React, { FC, memo } from "react";
+
+export const Page404: FC = memo(() => {
+  return <div>Page404</div>;
+});
---
+++ src/components/pages/Settings.tsx
@@ -0,0 +1,5 @@
+import React, { FC, memo } from "react";
+
+export const Settings: FC = memo(() => {
+  return <div>Settingsページ</div>;
+});
